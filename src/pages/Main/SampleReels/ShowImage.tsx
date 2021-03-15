23e19b3dcde972ccd56b33af669b4e67fb6b48c0
guangll <17600498920@163.com>
2021-03-15 21:33:52 +0800

    feat: update

--- src/pages/Main/SampleReels/ShowImage.tsx
+++ src/pages/Main/SampleReels/ShowImage.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './index.less';
 
 import { SearchOutlined } from '@ant-design/icons';
@@ -10,16 +11,33 @@ export type Props = {
 
 function Index(props: Props) {
   const { img, title, introduce } = props;
+  const [flag, setFlag] = useState(false);
+
+  const onMouseOver = (event: MouseEvent) => {
+    setFlag(true);
+  };
+
+  const onMouseOut = (event: MouseEvent) => {
+    setFlag(false);
+  };
+
   return (
     <div className={styles.single_img_box}>
-      <img className={styles.single_img} src={img} />
-      <div className={styles.single_img_hover}>
-        <p className={styles.single_title}>{title}</p>
-        <p className={styles.single_introduce}>{introduce}</p>
-        <p className={styles.single_icon}>
-          <SearchOutlined />
-        </p>
-      </div>
+      <img
+        className={styles.single_img}
+        src={img}
+        onMouseOver={onMouseOver}
+        onMouseOut={onMouseOut}
+      />
+      {flag && (
+        <div className={styles.single_img_hover}>
+          <p className={styles.single_title}>{title}</p>
+          <p className={styles.single_introduce}>{introduce}</p>
+          <p className={styles.single_icon}>
+            <SearchOutlined />
+          </p>
+        </div>
+      )}
     </div>
   );
 }
