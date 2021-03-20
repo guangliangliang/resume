1ca3fd2a140a001f8fa0519fe13cb6c53416bf97
guangll <17600498920@163.com>
2021-03-20 14:46:12 +0800

    update phone

--- src/pages/Main/HeaderMenu/index.tsx
+++ src/pages/Main/HeaderMenu/index.tsx
@@ -2,7 +2,7 @@ import styles from './index.less';
 import { menuData } from '@/pages/Main/HeaderMenu/config';
 import React, { useState } from 'react';
 import classNames from 'classnames';
-
+import { MenuFoldOutlined } from '@ant-design/icons';
 export type Props = {
   ifFixed?: boolean;
   clickLabel?: (label?: string) => void;
@@ -10,9 +10,12 @@ export type Props = {
 
 const Index: React.FC<Props> = (props) => {
   const { ifFixed, clickLabel } = props;
+  const [menuFlag, setMenuFlag] = useState(false);
+
   return (
     <div
       id="header_menu"
+      style={{ display: menuFlag ? 'block' : 'flex' }}
       className={classNames(styles.header_menu, ifFixed && styles.fixed)}
     >
       <div className={styles.header_menu_left}>
@@ -21,20 +24,49 @@ const Index: React.FC<Props> = (props) => {
         </a>
       </div>
       <div className={styles.header_menu_right}>
-        {menuData.map((item: { label: any }) => {
-          const { label } = item;
-          return (
-            <span
-              onClick={() => {
-                clickLabel(label);
-              }}
-              className={styles.right_label}
-            >
-              {label}
-            </span>
-          );
-        })}
+        <div className={styles.menu_right_web}>
+          {menuData.map((item: { label: any }) => {
+            const { label } = item;
+            return (
+              <span
+                onClick={() => {
+                  clickLabel(label);
+                }}
+                className={styles.right_label}
+              >
+                {label}
+              </span>
+            );
+          })}
+        </div>
+        <div className={styles.menu_right_phone}>
+          <MenuFoldOutlined
+            onClick={() => {
+              setMenuFlag(!menuFlag);
+            }}
+          />
+        </div>
       </div>
+      {menuFlag && (
+        <div className={styles.right_phone_ul}>
+          <ul>
+            {menuData.map((item: { label: any }) => {
+              const { label } = item;
+              return (
+                <li
+                  onClick={() => {
+                    setMenuFlag(false);
+                    clickLabel(label);
+                  }}
+                  className={styles.right_label}
+                >
+                  {label}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
